2451ef7f99307b7f7e9ed9924b151a4a767e0fe2
Dilmer Valecillos <dilmervst@gmail.com>
2023-04-14 14:03:40 -0600

    Removed Player property and XR Origin requirement from game manager

--- Assets/Scenes/EscapeRoom/Scripts/Managers/GameManager.cs
+++ Assets/Scenes/EscapeRoom/Scripts/Managers/GameManager.cs
@@ -1,18 +1,11 @@
 using DilmerGames.Core.Singletons;
 using System;
-using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    [Header("Game Manager Options")]
-    [SerializeField]
-    private XROrigin xrOrigin;
-
     [field: SerializeField]
     public GameState GameState { get; private set; } = GameState.Playing;
-
-    public XROrigin Player { get { return xrOrigin; } }
 
     [Header("Events")]
     public Action<GameState> OnGamePaused;
@@ -23,7 +16,7 @@ public class GameManager : Singleton<GameManager>
 
     private void Awake()
     {
-        cachedCameraCullingMask = xrOrigin.Camera.cullingMask;
+        cachedCameraCullingMask = Camera.main.cullingMask;
         ControllerManager.Instance.OnControllerMenuActionExecuted += ChangeGameState;
         UIManager.Instance.OnGameResumeActionExecuted += ChangeGameState;
     }
@@ -42,13 +35,13 @@ public class GameManager : Singleton<GameManager>
         {
             OnGamePaused?.Invoke(GameState.Paused);
             Time.timeScale = 0;
-            xrOrigin.Camera.cullingMask = LayerMask.GetMask("UI");
+            Camera.main.cullingMask = LayerMask.GetMask("UI");
         }
         else
         {
             OnGameResumed?.Invoke(GameState.Playing);
             Time.timeScale = 1;
-            xrOrigin.Camera.cullingMask = cachedCameraCullingMask;
+            Camera.main.cullingMask = cachedCameraCullingMask;
         }
     }
 }
